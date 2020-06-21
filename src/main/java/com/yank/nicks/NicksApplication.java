29f9ad93cb6de30963f40a762aa43c566b43f601
higorcemianko <higor.cemianko@gmail.com>
2020-06-21 16:04:01 -0300

    subindo arquivos compilacao

--- src/main/java/com/yank/nicks/NicksApplication.java
+++ src/main/java/com/yank/nicks/NicksApplication.java
@@ -9,7 +9,6 @@ public class NicksApplication {
 	private static final String URL_GERACAO_NICKS = "https://www.4devs.com.br/gerador_de_nicks";
 	private static final String URL_GERACAO_CPFS = "https://www.4devs.com.br/gerador_de_cpf";
 	public static void main(String[] args) {
-		System.out.println("criando jar");
 		List<String> nicks = new GerarNicks().getNicks(URL_GERACAO_NICKS);
 		Map<String, String> nickCpf = new GerarCpfs().getCpfs(nicks, URL_GERACAO_CPFS);
 		new GerarArquivoTexto().gravarNickCpf(nickCpf);
